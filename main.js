8b5701e2fde20a78a85e9e46147d4cd1e7dee82e
bek123456789 <redmirating@gmail.com>
2024-03-19 10:30:51 +0500

    done

---
+++ main.js
@@ -0,0 +1,23 @@
+let first = (Math.round(Math.random() * 100))
+
+alert(`${'Random  son: '}${first}`)
+
+
+if (first % 3 === 0 && first % 5 === 0) {
+    alert(first + ' :fizzbuzz')
+}
+
+
+else if (first % 3 === 0) {
+    alert(first + ' :fizz')
+}
+
+else if (first % 5 === 0) {
+    alert(first + ' :buzz')
+}
+
+
+
+else {
+    alert(first + ' soni fizz ham buzz ham fizzbuzz ham emas')
+}
